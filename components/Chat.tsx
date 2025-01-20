f106fe8de812beb5a2decf3e03fe8b0ae7e71454
gustavokurtz <kurtzff32@gmail.com>
2025-01-20 18:18:19 -0300

    up

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -22,9 +22,7 @@ export default function Chat() {
     setLoading(true);
 
     try {
-      const res = await axios.post("http://159.65.44.185:3001/detectar", { text: message }, { 
-        headers: { "Content-Security-Policy": "upgrade-insecure-requests" }
-      });
+      const res = await axios.post("https://159.65.44.185:3001/detectar", { text: message });
 
       // Garantimos que a resposta seja um objeto antes de salvar
       const responseData = typeof res.data === "string" ? JSON.parse(res.data) : res.data;
